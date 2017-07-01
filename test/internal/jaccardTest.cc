b8de7148c98ec1ea8a2ca2257e3317a8c69b716a
fruttasecca <jacopo.gobbi@studenti.unitn.it>
2017-07-01 14:12:25 +0200

    added jaccard tests

---
+++ test/internal/jaccardTest.cc
@@ -0,0 +1,106 @@
+#include <iostream>
+#include <unordered_set>
+
+#include <gtest.h>
+#include <gtest-spi.h>
+#include <pprInternal.h>
+
+using namespace std;
+using ppr::pprInternal::jaccard;
+using std::unordered_set;
+
+TEST(testElements, testEmpty)
+{
+  unordered_set<int> s1, s2;
+  ASSERT_EQ(jaccard(s1,s2), 1.0);
+}
+
+TEST(testElements, testOneElement1)
+{
+  unordered_set<int> s1, s2;
+  s1.insert(2);
+  ASSERT_EQ(jaccard(s1,s2), 0.0);
+}
+
+TEST(testElements, testOneElement1_1)
+{
+  unordered_set<int> s1, s2;
+  s2.insert(2);
+  ASSERT_EQ(jaccard(s1,s2), 0.0);
+}
+
+TEST(testElements, testOneElement2)
+{
+  unordered_set<int> s1, s2;
+  s1.insert(2);
+  s2.insert(-2);
+  ASSERT_EQ(jaccard(s1,s2), 0.0);
+}
+
+TEST(testElements, testOneElement3)
+{
+  unordered_set<int> s1, s2;
+  s1.insert(-2);
+  s2.insert(-2);
+  ASSERT_EQ(jaccard(s1,s2), 1.0);
+}
+
+TEST(testSets, testIdenticallyPopulatedSets)
+{
+  unordered_set<int> s1, s2;
+  for(int i = 0; i < 100; i++)
+  {
+    s1.insert(i);
+    s2.insert(i);
+  }
+  ASSERT_EQ(jaccard(s1,s2), 1.0);
+}
+
+TEST(testSets, testIdenticalSets)
+{
+  unordered_set<int> s1, s2;
+  for(int i = 0; i < 100; i++)
+  {
+    s1.insert(i);
+    s2.insert(i);
+  }
+  ASSERT_EQ(jaccard(s1,s2), 1.0);
+}
+
+TEST(testSets, testTotallyDifferentSets)
+{
+  unordered_set<int> s1, s2;
+  for(int i = 1; i < 100; i++)
+  {
+    s1.insert(i);
+    s2.insert(-i);
+  }
+  ASSERT_EQ(jaccard(s1,s2), 0.0);
+}
+
+TEST(testSets, testHalfJaccard)
+{
+  unordered_set<int> s1, s2;
+  for(int i = 0; i < 100; i++)
+  {
+    s1.insert(i);
+    s2.insert(i + 100);
+  }
+  s2.insert(s1.begin(), s2.end());
+  ASSERT_EQ(jaccard(s1,s2), 0.5);
+}
+
+TEST(testSets, testPercentJaccard)
+{
+  unordered_set<int> s1, s2;
+  //init a set with ints from 0 to 99
+  for(int i = 0; i < 100; i++)
+    s1.insert(i);
+  //grow s2 10% at a time
+  for(int round = 1; round <= 10; round++)
+  {
+      for(int i = (round - 1) * 10 ; i < round * 10; i++)
+          s2.insert(i);
+      ASSERT_EQ(jaccard(s1,s2), round/10.0);
+  }
+}

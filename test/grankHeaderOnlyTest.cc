4d6a85631fbf51546967ee8e5b8a1f0291d7f69a
fruttasecca <jacopo.gobbi@studenti.unitn.it>
2017-07-03 18:06:46 +0200

    added tests for grank header only (same as grank tests)

---
+++ test/grankHeaderOnlyTest.cc
@@ -0,0 +1,379 @@
+#include <unordered_set>
+#include <unordered_map>
+#include <vector>
+#include <stdlib.h>//exit
+#include <random>
+
+#include <gtest.h>
+#include <gtest-spi.h>
+#include <../header-only/grank.h>
+#include <pprSingleSource.h>
+
+using namespace std;
+using ppr::grank;
+using ppr::pprInternal::pprSingleSource;
+
+extern std::random_device rd;
+extern std::default_random_engine eng;
+extern std::uniform_int_distribution<unsigned long long> dis;
+
+TEST(grankHeaderOnly, badParameters)
+{
+  unordered_map<int, vector<int>> graph;
+  ASSERT_EXIT(grank(graph, 0, 3, 42, 0.5, 0.0001), ::testing::ExitedWithCode(EXIT_FAILURE), "K must be positive");
+  ASSERT_EXIT(grank(graph, 2, 0, 32, 0.85, 0.0001), ::testing::ExitedWithCode(EXIT_FAILURE), "L must be positive");
+  ASSERT_EXIT(grank(graph, 2, 1, 10, 0.5, 0.0001), ::testing::ExitedWithCode(EXIT_FAILURE), "K must be <= L");
+  ASSERT_EXIT(grank(graph, 2, 2, 0, 0.5, 0.0001), ::testing::ExitedWithCode(EXIT_FAILURE), "iterations must be positive");
+  ASSERT_EXIT(grank(graph, 2, 2, 10, 1.5, 0.0001), ::testing::ExitedWithCode(EXIT_FAILURE), "damping must be \\[0,1]");
+  ASSERT_EXIT(grank(graph, 2, 2, 10, -1.5, 0.0001), ::testing::ExitedWithCode(EXIT_FAILURE), "damping must be \\[0,1]");
+}
+
+TEST(grankHeaderOnly, emptyGraph)
+{
+  unordered_map<int, vector<int>> graph;
+  auto res =  grank(graph, 10, 30, 100, 0.85, 0.0001);
+  ASSERT_EQ(res.size(), 0);
+}
+
+TEST(grankHeaderOnly, testNoEdges)
+{
+  unordered_map<int, vector<int>> graph;
+  for(int i = 0; i < 10; i++)
+    graph[i];
+  auto res =  grank(graph, 10, 30, 100, 0.85, 0.0001);
+  ASSERT_EQ(res.size(), graph.size());
+  for(int i = 0; i < 10; i++)
+  {
+    ASSERT_EQ(res[i].size(), 1);
+    ASSERT_NEAR(res[i][i], 0.15, 10e-5);
+  }
+}
+
+TEST(grankHeaderOnly, testTopL)
+{
+  unordered_map<int, vector<int>> graph;
+  //adds 30 nodes and <= 30 random edges, then checks if the returned top
+  //for each node is always <= topL
+  for(int i = 0; i < 30; i++)
+    graph[i];
+  for(int i = 0; i < 30; i++)
+    graph[dis(eng)%30].push_back(dis(eng)%30);
+  for(int i = 1; i < 30; i++)
+  {
+    auto res =  grank(graph, i, i, 100, 0.85, 0.0001);
+    ASSERT_EQ(res.size(), graph.size());
+    for(int u = 0; u < 30; u++)
+      ASSERT_LE(res[u].size(), i);
+  }
+}
+
+TEST(grankHeaderOnly, singleNode)
+{
+  unordered_map<int, vector<int>> graph;
+  graph[0];
+  auto res =  grank(graph, 10, 30, 100, 0.85, 0.0001);
+  ASSERT_EQ(res.size(), graph.size());
+  ASSERT_EQ(res[0].size(), graph.size());
+  ASSERT_NEAR(res[0][0], 0.15, 10e-5);
+
+  graph[0].push_back(0);
+  res =  grank(graph, 10, 30, 100, 0.85, 0.0001);
+  ASSERT_EQ(res.size(), graph.size());
+  ASSERT_EQ(res[0].size(), graph.size());
+  ASSERT_NEAR(res[0][0], 1.0, 10e-5);
+}
+
+TEST(grankHeaderOnly, twoNodes)
+{
+  unordered_map<int, vector<int>> graph;
+  graph[0];graph[1];
+  auto res =  grank(graph, 10, 30, 100, 0.85, 0.0001);
+  ASSERT_EQ(res.size(), graph.size());
+  ASSERT_EQ(res[0].size(), 1);
+  ASSERT_EQ(res[1].size(), 1);
+  ASSERT_NEAR(res[0][0], 0.15, 10e-5);
+  ASSERT_NEAR(res[1][1], 0.15, 10e-5);
+
+  graph[0].push_back(1);
+  graph[1].push_back(0);
+  res =  grank(graph, 10, 30, 100, 0.85, 0.0001);
+  ASSERT_EQ(res.size(), graph.size());
+  ASSERT_EQ(res[0].size(), graph.size());
+  ASSERT_EQ(res[1].size(), graph.size());
+  ASSERT_GT(res[0][0], res[0][1]);
+  ASSERT_GT(res[1][1], res[1][0]);
+}
+
+TEST(grankHeaderOnly, lineGraph)
+{
+  unordered_map<int, vector<int>> graph;
+  for(int i = 0; i < 6; i++)
+    graph[i];
+
+  for(int i = 0; i < 6; i++)
+    graph[i].push_back((i+1)%6);
+
+  auto res =  grank(graph, 10, 30, 100, 0.85, 0.0001);
+  ASSERT_EQ(res.size(), graph.size());
+
+  //for each node check that the PPR of a node after is always lower
+  for(int i = 0; i < 6; i++)
+  {
+      ASSERT_EQ(res[i].size(), graph.size());
+      for(int u = 0; u < 5; u++)
+        ASSERT_GT(res[i][(i + u)%6], res[i][(i + u + 1)%6]);
+  }
+
+  ASSERT_EQ(res.size(), graph.size());
+  res =  grank(graph, 3, 4, 100, 0.85, 0.0001);
+
+  //for each node check that the PPR of a node after is always lower
+  for(int i = 0; i < 6; i++)
+  {
+      ASSERT_EQ(res[i].size(), 3);
+      for(int u = 0; u < 2; u++)
+      {
+        ASSERT_GT(res[i][(i + u)%6],res[i][(i + u + 1)%6]);
+      }
+  }
+
+  ASSERT_EQ(res.size(), graph.size());
+  res =  grank(graph, 3, 3, 100, 0.85, 0.0001);
+
+  //for each node check that the PPR of a node after is always lower
+  for(int i = 0; i < 6; i++)
+  {
+      ASSERT_EQ(res[i].size(), 3);
+      for(int u = 0; u < 2; u++)
+      {
+        ASSERT_GT(res[i][(i + u)%6],res[i][(i + u + 1)%6]);
+      }
+  }
+}
+
+TEST(grankHeaderOnly, starGraph)
+{
+  unordered_map<int, vector<int>> graph;
+  for(int i = 0; i < 6; i++)
+    graph[i];
+
+  for(int i = 1; i < 6; i++)
+    graph[i].push_back(0);
+
+  auto res =  grank(graph, 10, 30, 100, 0.85, 0.0001);
+  ASSERT_EQ(res.size(), graph.size());
+  ASSERT_EQ(res[0].size(), 1);
+  ASSERT_NEAR(res[0][0], 0.15, 10e-5);
+
+  for(int i = 1; i < 6; i++)
+  {
+    ASSERT_EQ(res[i].size(), 2);
+    ASSERT_NEAR(res[i][0], 0.15 * 0.85, 10e-5);
+  }
+
+  //connect the center to itself
+  graph[0].push_back(0);
+  res =  grank(graph, 10, 30, 100, 0.85, 0.0001);
+  for(int i = 1; i < 6; i++)
+  {
+    ASSERT_EQ(res[i].size(), 2);
+    ASSERT_NEAR(res[i][0], 0.85, 10e-5);
+  }
+}
+
+TEST(grankHeaderOnly, testNodesGreaterThanK)
+{
+  const size_t K = 10;
+  unordered_map<int, vector<int>> graph;
+  for(int i = 0; i < 100; i++)
+    graph[i];
+  for(int i = 0; i < 99; i++)
+    graph[i].push_back(i + 1);
+  graph[99].push_back(0);
+
+  auto res =  grank(graph, K, K, 100, 0.85, 0.0001);
+
+  //for each node check that the PPR of a node after that is always lower,
+  for(int i = 0; i < 100; i++)
+  {
+      ASSERT_EQ(res[i].size(), K);
+      for(int u = 0, uEnd = K - 1; u < uEnd; u++)
+      {
+        ASSERT_GT(res[i][(i + u + 1)%100], 0);
+        ASSERT_GT(res[i][(i + u)%100], res[i][(i + u + 1)%100]);
+      }
+  }
+
+  //same as before but with L > K
+  res =  grank(graph, K, K * 2, 100, 0.85, 0.0001);
+
+  //for each node check that the PPR of a node after that is always lower,
+  for(int i = 0; i < 100; i++)
+  {
+      ASSERT_EQ(res[i].size(), K);
+      for(int u = 0, uEnd = K - 1; u < uEnd; u++)
+      {
+        ASSERT_GT(res[i][(i + u + 1)%100], 0);
+        ASSERT_GT(res[i][(i + u)%100], res[i][(i + u + 1)%100]);
+      }
+  }
+
+  //same as before but with L  = graph.size()
+  res =  grank(graph, K, graph.size(), 100, 0.85, 0.0001);
+
+  //for each node check that the PPR of a node after that is always lower,
+  for(int i = 0; i < 100; i++)
+  {
+      ASSERT_EQ(res[i].size(), K);
+      for(int u = 0, uEnd = K - 1; u < uEnd; u++)
+      {
+        ASSERT_GT(res[i][(i + u + 1)%100], 0);
+        ASSERT_GT(res[i][(i + u)%100], res[i][(i + u + 1)%100]);
+      }
+  }
+
+}
+
+TEST(grankHeaderOnly, testNodesEqualK)
+{
+  unordered_map<int, vector<int>> graph;
+  for(int i = 0; i < 100; i++)
+    graph[i];
+  for(int i = 0; i < 99; i++)
+    graph[i].push_back(i + 1);
+  graph[99].push_back(0);
+
+  //-1 tolerance means no tolerance/ignored
+  auto res =  grank(graph, graph.size(), graph.size(), 100, 0.85, -1);
+
+  //for each node check that the PPR of a node after that is always lower,
+  for(int i = 0; i < 100; i++)
+  {
+      ASSERT_EQ(res[i].size(), graph.size());
+      for(int u = 0, uEnd = graph.size() - 1; u < uEnd; u++)
+      {
+        ASSERT_GT(res[i][(i + u + 1)%100], 0);
+        ASSERT_GT(res[i][(i + u)%100], res[i][(i + u + 1)%100]);
+      }
+  }
+}
+
+TEST(grankHeaderOnly, testNodesLowerThanK)
+{
+  unordered_map<int, vector<int>> graph;
+  for(int i = 0; i < 100; i++)
+    graph[i];
+  for(int i = 0; i < 99; i++)
+    graph[i].push_back(i + 1);
+  graph[99].push_back(0);
+
+  //-1 tolerance means no tolerance/ignored
+  auto res =  grank(graph, graph.size() * 2, graph.size() * 2, 100, 0.85, -1);
+
+  //for each node check that the PPR of a node after that is always lower,
+  for(int i = 0; i < 100; i++)
+  {
+      ASSERT_EQ(res[i].size(), graph.size());
+      for(int u = 0, uEnd = graph.size() - 1; u < uEnd; u++)
+      {
+        ASSERT_GT(res[i][(i + u + 1)%100], 0);
+        ASSERT_GT(res[i][(i + u)%100], res[i][(i + u + 1)%100]);
+      }
+  }
+}
+
+TEST(grankHeaderOnly, sameAsPagerank1)
+{
+  unordered_map<int, vector<int>> graph;
+  for(int i = 0; i < 100; i++)
+    graph[i];
+  for(int i = 0; i < 99; i++)
+    graph[i].push_back(i + 1);
+  graph[99].push_back(0);
+
+  auto gr =  grank(graph, graph.size(), graph.size(), 100, 0.85, -1);
+  for(int i = 0; i < 100; i++)
+  {
+    auto ppr = pprSingleSource(graph, 100, 0.85, -1,  i);
+    ASSERT_EQ(gr[i].size(), ppr.size());
+    for(int u = 0; u < 100; u++)
+      ASSERT_NEAR(gr[i][u], ppr[u], 10e-5);
+  }
+}
+
+TEST(grankHeaderOnly, sameAsPagerank2)
+{
+  unordered_map<int, vector<int>> graph;
+  for(int i = 0; i < 100; i++)
+    graph[i];
+  for(int i = 0; i < 99; i++)
+    graph[i].push_back(0);
+
+  auto gr =  grank(graph, graph.size(), graph.size(), 100, 0.85, -1);
+  for(int i = 0; i < 100; i++)
+  {
+    auto ppr = pprSingleSource(graph, 100, 0.85, -1,  i);
+    ASSERT_EQ(gr[i].size(), ppr.size());
+    for(int u = 0; u < 100; u++)
+      ASSERT_NEAR(gr[i][u], ppr[u], 10e-5);
+  }
+
+  graph[0].push_back(0);
+  gr =  grank(graph, graph.size(), graph.size(), 100, 0.85, -1);
+  for(int i = 0; i < 100; i++)
+  {
+    auto ppr = pprSingleSource(graph, 100, 0.85, -1,  i);
+    ASSERT_EQ(gr[i].size(), ppr.size());
+    for(int u = 0; u < 100; u++)
+      ASSERT_NEAR(gr[i][u], ppr[u], 10e-5);
+  }
+
+  for(int i = 0; i < 99; i++)
+    graph[0].push_back(i);
+  gr =  grank(graph, graph.size(), graph.size(), 100, 0.85, -1);
+  for(int i = 0; i < 100; i++)
+  {
+    auto ppr = pprSingleSource(graph, 100, 0.85, -1,  i);
+    ASSERT_EQ(gr[i].size(), ppr.size());
+    for(int u = 0; u < 100; u++)
+      ASSERT_NEAR(gr[i][u], ppr[u], 10e-5);
+  }
+}
+
+TEST(grankHeaderOnly, sameAsPagerank3)
+{
+  unordered_map<int, vector<int>> graph;
+  int n = 100;
+  int edges = 5000;
+  for(int i = 0; i < n; i++)
+    graph[i];
+  for(int i = 0; i < edges; i++)
+    graph[dis(eng)%n].push_back(dis(eng)%n);
+
+  auto gr =  grank(graph, graph.size(), graph.size(), 100, 0.85, -1);
+  for(int i = 0; i < n; i++)
+  {
+    auto ppr = pprSingleSource(graph, 100, 0.85, -1,  i);
+    ASSERT_EQ(gr[i].size(), ppr.size());
+    for(int u = 0; u < n; u++)
+      ASSERT_NEAR(gr[i][u], ppr[u], 10e-5);
+  }
+}
+
+TEST(grankHeaderOnly, sameAsPagerank4)
+{
+  unordered_map<int, vector<int>> graph;
+  int n = 100;
+  for(int i = 0; i < n; i++)
+    for(int u = 0; u < n; u++)
+      graph[i].push_back(u);
+
+  auto gr =  grank(graph, graph.size(), graph.size(), 100, 0.85, -1);
+  for(int i = 0; i < n; i++)
+  {
+    auto ppr = pprSingleSource(graph, 100, 0.85, -1,  i);
+    ASSERT_EQ(gr[i].size(), ppr.size());
+    for(int u = 0; u < n; u++)
+      ASSERT_NEAR(gr[i][u], ppr[u], 10e-5);
+  }
+}

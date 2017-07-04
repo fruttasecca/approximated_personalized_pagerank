45b285a0f9bcb7d905db6c0da0c25dc5bbb092d5
fruttasecca <jacopo.gobbi@studenti.unitn.it>
2017-07-04 08:53:21 +0200

    fixed import

--- src/main.cc
+++ src/main.cc
@@ -8,12 +8,14 @@
 #include <chrono>
 
 #include <kendall.h>
-#include <../header-only/grank.h>
+#include <grank.h>
+#include <mccompletepathv2.h>
 #include <pprSingleSource.h>
 #include <benchmarkAlgorithm.h>
 
 using namespace std;
 using ppr::grank;
+using ppr::mccompletepathv2;
 using ppr::pprInternal::pprSingleSource;
 using ppr::benchmarkAlgorithm;
 
@@ -23,13 +25,15 @@ using ppr::benchmarkAlgorithm;
  * node1, node2
  */
 unordered_map<int, vector<int>> importGraph(string fname);
+unordered_map<string, vector<string>> importGraph2(string fname);
 
 int main()
 {
   unordered_map<int, vector<int>> graph = importGraph("example.txt");
+  //unordered_map<string, vector<string>> graph = importGraph2("example.txt");
 
   auto begin = std::chrono::steady_clock::now();
-  auto map = grank<int>(graph, 50, 100, 30, 0.85, 0.0001);
+  auto map = grank(graph, 50, 100, 30, 0.85, 0.0001);
   auto end= std::chrono::steady_clock::now();
   std::cout << "grank run-time = " << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << " ms" << endl;
 
@@ -56,9 +60,47 @@ unordered_map<int, vector<int>> importGraph(string fname)
   while(getline(inputFile, tmp))
   {
     size_t pos = tmp.find(delimiter);
-
+    tmp.erase(std::remove(tmp.begin(), tmp.end(), '\r'), tmp.end());
+    tmp.erase(std::remove(tmp.begin(), tmp.end(), '\n'), tmp.end());
     int n1 = stoi(tmp.substr(0, pos));
     int n2 = stoi(tmp.substr(pos + 1));
+
+    //make sure that the second node is added to the map
+    //in case there are no edges starting from this node
+    graph[n2];
+
+    //checking to avoid repeated edges in the csv file
+    if(!edges[n1][n2])
+    {
+      edges[n1][n2] = true;
+      graph[n1].push_back(n2);
+      edgeCounter++;
+    }
+  }
+  cout << "nodes: " << graph.size() << " edges: " << edgeCounter << endl;
+
+  return graph;
+}
+
+unordered_map<string, vector<string>> importGraph2(string fname)
+{
+  size_t edgeCounter = 0;
+  ifstream inputFile(fname, ifstream::in);
+  unordered_map<string, vector<string>> graph;
+
+  //needed for repeating edges
+  unordered_map<string, unordered_map<string, bool>> edges;
+
+  string tmp = "";
+  string delimiter = ",";
+  while(getline(inputFile, tmp))
+  {
+    size_t pos = tmp.find(delimiter);
+    tmp.erase(std::remove(tmp.begin(), tmp.end(), '\r'), tmp.end());
+    tmp.erase(std::remove(tmp.begin(), tmp.end(), '\n'), tmp.end());
+    string n1 = tmp.substr(0, pos);
+    string n2 = tmp.substr(pos + 1);
+
 
     //make sure that the second node is added to the map
     //in case there are no edges starting from this node
